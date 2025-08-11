5dac1917040384ea3f2611ab0163d1aee051192d
Erick Otenyo <otenyo.erick@gmail.com>
2025-08-11 19:11:51 +0300

    Bump version

--- plugins/adl_weatherlink_plugin/setup.py
+++ plugins/adl_weatherlink_plugin/setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 PROJECT_DIR = os.path.dirname(__file__)
 REQUIREMENTS_DIR = os.path.join(PROJECT_DIR, "requirements")
-VERSION = "0.0.1"
+VERSION = "0.1.0"
 
 
 def get_requirements(env):
